dd74c949080f6fb37b8dd1ea71efd0b264b79f36
Moatassem Seghyar <moatassem.seghyar@grenoble-inp.org>
2026-08-19 13:34:20 +0200

    fix: missing slash

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
+	"path/filepath"
 	"strconv"
+	"strings"
 )
 
 type record struct {
-	absolutePath string;
-	pids []int;
-	availableControllers []string;
+	absolutePath         string
+	pids                 []int
+	availableControllers []string
 }
 
 func parse_directory(path string, depth int, records []record) []record {
@@ -25,15 +26,13 @@ func parse_directory(path string, depth int, records []record) []record {
 	var controllers = []string{}
 	var pids = []int{}
 
-
 	content, err := os.ReadFile(path + "/cgroup.procs")
 
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 	} else {
 		pidsStr := strings.Fields(string(content))
-		fmt.Printf("============= LE CONTENU DE CGROUP.PROCS POUR %v EST:\n %v\n\n", path, string(content))
-		for _ ,pid := range(pidsStr) {
+		for _, pid := range pidsStr {
 			pidInt, _ := strconv.Atoi(pid)
 			pids = append(pids, pidInt)
 		}
@@ -47,36 +46,35 @@ func parse_directory(path string, depth int, records []record) []record {
 
 	}
 
-	for _, v := range(files) {
+	for _, v := range files {
 		for range depth {
 			fmt.Printf("\t")
 		}
 		fmt.Printf("%v\n", v)
 		if v.IsDir() {
-			records = parse_directory(path + v.Name(), depth + 1, records)
+			records = parse_directory(filepath.Join(path, v.Name()), depth+1, records)
 		}
 	}
-	
+
 	currentRecord.pids = pids
 	currentRecord.absolutePath = path
 	currentRecord.availableControllers = controllers
 
 	records = append(records, currentRecord)
-	
+
 	return records
 }
-
 
 func main() {
 	var dir string = "/sys/fs/cgroup"
 	if len(os.Args) > 1 {
 		dir = os.Args[1]
 	}
-	
+
 	records := []record{}
 
 	records = parse_directory(dir, 1, records)
-	for _, v := range(records) {
-		fmt.Println(v)
-	}
+	// for _, v := range records {
+	// 	fmt.Println(v)
+	// }
 }
