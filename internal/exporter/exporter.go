63b37f66026a6d18796159beaa97e409caeec9bb
Moatassem Seghyar <moatassem.seghyar@grenoble-inp.org>
2026-08-22 21:30:59 +0200

    feat: add -prom option for Prometheus integration

---
+++ internal/exporter/exporter.go
@@ -0,0 +1,72 @@
+package exporter
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/SMoatassem/cgroups-stat/internal/cgroup"
+)
+
+func ExportMetrics (w http.ResponseWriter, r *http.Request) {
+	
+	w.Header().Set("Content-Type", "text/plain; version=0.0.4; charset=utf-8")
+
+	records := []cgroup.Record{}
+	fields := []string{"MemoryCurrent", "usageUsec", "throttledUsec", "nrThrottled", "nrPeriods"}
+
+	// If prometheus is used, we only go to collect 
+	// metrics from the /sys/fs/cgroup directory 
+	records = cgroup.ParseDirectory("/sys/fs/cgroup", 1, records, false, false)
+
+	for _, field := range(fields) {
+		switch field {
+			case "MemoryCurrent": 
+				fmt.Fprintf(w, "# HELP cgstat_memory_usage_current_bytes Current memory usage in bytes\n")
+				fmt.Fprintf(w, "# TYPE cgstat_memory_usage_current_total gauge\n")
+			case "usageUsec":
+				fmt.Fprintf(w, "# HELP cgstat_cpu_usage_seconds_total Current CPU usage in seconds\n")
+				fmt.Fprintf(w, "# TYPE cgstat_cpu_usage_seconds_total counter\n")
+			case "throttledUsec":
+				fmt.Fprintf(w, "# HELP cgstat_cpu_throttled_seconds_total Current CPU throttle in seconds\n")
+				fmt.Fprintf(w, "# TYPE cgstat_cpu_throttled_seconds_total counter\n")
+
+			case "nrThrottled":
+				fmt.Fprintf(w, "# HELP cgstat_cpu_throttled_periods_total Current CPU throttle periods\n")
+				fmt.Fprintf(w, "# TYPE cgstat_cpu_throttled_periods_total counter\n")
+			case "nrPeriods": 
+				fmt.Fprintf(w, "# HELP cgstat_cpu_periods_total Current CPU periods\n")
+				fmt.Fprintf(w, "# TYPE cgstat_cpu_periods_total counter\n")
+		}
+		for _, record := range(records) {
+			switch field {
+				case "MemoryCurrent":
+					fmt.Fprintf(w, "cgstat_memory_usage_current_bytes{cgroup=%q} %v\n", record.AbsolutePath, record.MemoryCurrent)
+
+				case "usageUsec":
+					value, ok := record.CpuStat["usageUsec"]
+					if ok {
+						fmt.Fprintf(w, "cgstat_cpu_usage_seconds_total{cgroup=%q} %f\n", record.AbsolutePath, float64(value)/float64(1_000_000))
+					}
+					
+				case "throttledUsec":
+					value , ok := record.CpuStat["throttledUsec"]
+					if ok {
+						fmt.Fprintf(w, "cgstat_cpu_throttled_seconds_total{cgroup=%q} %f\n", record.AbsolutePath, float64(value)/float64(1_000_000))	
+					}
+				
+				case "nrThrottled":
+					value, ok := record.CpuStat["nrThrottled"]
+					if ok {
+						fmt.Fprintf(w, "cgstat_cpu_throttled_periods_total{cgroup=%q} %v\n", record.AbsolutePath, value)
+					}
+					
+				case "nrPeriods":
+					value, ok := record.CpuStat["nrPeriods"]
+					if ok {
+						fmt.Fprintf(w, "cgstat_cpu_periods_total{cgroup=%q} %v\n", record.AbsolutePath, value)
+					}
+			}
+		}
+		fmt.Fprintf(w, "\n")
+	}
+}
--- main.go
+++ main.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
 	"github.com/SMoatassem/cgroups-stat/internal/cgroup"
-	"github.com/SMoatassem/cgroups-stat/internal/rate"
+	"github.com/SMoatassem/cgroups-stat/internal/exporter"
 	"github.com/SMoatassem/cgroups-stat/internal/output"
+	"github.com/SMoatassem/cgroups-stat/internal/rate"
 )
 
 
@@ -20,42 +24,73 @@ func main() {
 	verbose := flag.Bool("v", false, "Enable verbosity to view errors")
 	sortKey := flag.String("sort", "cores", "Sort table by: cores | throttle | periods | memory")
 	maxLines := flag.Int("n", 20 , "Change the number of lines shown in the table")
+	prom := flag.Bool("prom", false, "Enable http server for Prometheus exportation")
 	flag.Parse()
 
 	const clearScreen = "\033[H\033[2J\033[3J"
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
 	var dir string = *path
 
-	records := []cgroup.Record{}
+	if ! *prom {
+		records := []cgroup.Record{}
 
-	prev := cgroup.ParseDirectory(dir, 1, records, *tree, *verbose)
-	if *verbose {
-		for _, v := range prev {
-			fmt.Println(v)
-		}
-	}
-	
-	for {
-		select {
-		case <- ticker.C: 
-			curr := cgroup.ParseDirectory(dir, 1, records, *tree, *verbose)
-			
-			samples := rate.ComputeRates(prev, curr)
-			
-			fmt.Print(clearScreen)
-			output.PrintSamples(samples, dir, *sortKey, *maxLines)
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
 		
-			prev = curr
+		prev := cgroup.ParseDirectory(dir, 1, records, *tree, *verbose)
 		
-		case <- ctx.Done():
-			return
+		if *verbose {
+			for _, v := range prev {
+				fmt.Println(v)
+			}
 		}
+
+		for {
+			select {
+			case <- ticker.C: 
+				curr := cgroup.ParseDirectory(dir, 1, records, *tree, *verbose)
+				
+				samples := rate.ComputeRates(prev, curr)
+				
+				fmt.Print(clearScreen)
+				output.PrintSamples(samples, dir, *sortKey, *maxLines)
+			
+				prev = curr
+			
+			case <- ctx.Done():
+				return
+			}
+		}
+	} else {
+		mux := http.NewServeMux()
+		mux.HandleFunc("/metrics", exporter.ExportMetrics)
+		srv := &http.Server{
+			Addr:              ":9100",
+			Handler:           mux,
+			ReadHeaderTimeout: 5 * time.Second,
+		}
+
+		go func () {
+			fmt.Println("starting HTTP server.. ")
+			err := srv.ListenAndServe()
+
+			if err != nil && err != http.ErrServerClosed {
+				log.Printf("HTTP server couldn't start, error %v\n", err)
+				stop()
+			}
+
+		}()
+
+		<-ctx.Done()
+
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		_ = srv.Shutdown(shutdownCtx)
 	}
+
 	
 
 	
